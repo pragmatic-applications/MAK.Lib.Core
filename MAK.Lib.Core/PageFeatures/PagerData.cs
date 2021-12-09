4ae8c6a08afeed6d8e590ff148bfc91b06856ffc
Maurice Kelly <kay86@live.com>
2021-12-09 13:38:59 +0000

    Update PagerData and PagingLink of PageFeatures.

--- MAK.Lib.Core/PageFeatures/PagerData.cs
+++ MAK.Lib.Core/PageFeatures/PagerData.cs
@@ -6,8 +6,6 @@ public class PagerData
     public int TotalPages { get; set; }
     public int PageSize { get; set; }
     public int TotalCount { get; set; }
-
-    public bool IsCursorPointer => this.CurrentPage > 1;
     public bool HasPrevious => this.CurrentPage > 1;
     public bool HasNext => this.CurrentPage < this.TotalPages;
 }
--- MAK.Lib.Core/PageFeatures/PagingLink.cs
+++ MAK.Lib.Core/PageFeatures/PagingLink.cs
@@ -2,17 +2,18 @@
 
 public class PagingLink
 {
-    public string Text { get; set; }
-    public int Page { get; set; }
-    public bool Enabled { get; set; }
-    public bool Active { get; set; }
-    public bool IsCursorPointer { get; set; }
-
-    public PagingLink(int page, bool enabled, string text, bool isCursorPointer = false)
+    public PagingLink(int page, bool enabled, string text, bool isCursorPointer = false, bool active = false)
     {
         this.Page = page;
         this.Enabled = enabled;
         this.IsCursorPointer = isCursorPointer;
         this.Text = text;
+        this.Active = active;
     }
+
+    public string Text { get; set; }
+    public int Page { get; set; }
+    public bool Enabled { get; set; }
+    public bool Active { get; set; }
+    public bool IsCursorPointer { get; set; }
 }
